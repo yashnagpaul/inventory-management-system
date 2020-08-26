1a5d62cad4a4b4dcffbb15f316f97f4ac74873d2
rhaleem <reza.haleem@gmail.com>
2020-08-26 15:54:12 -0700

    worked on the list warehouses front-end and back-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles/App.css";
+import axios from "axios";
+
 import Header from "./components/Header";
-import AddWarehouse from "./components/AddWarehouse";
-import EditWarehouse from "./components/EditWarehouse";
+// import AddWarehouse from "./components/AddWarehouse";
+// import EditWarehouse from "./components/EditWarehouse";
 import ListWarehouses from "./components/ListWarehouses";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      {/* <AddWarehouse />
+class App extends Component {
+  state = {
+    warehouses: [],
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:8080/api/warehouses").then((response) => {
+      console.log("Warehouse List",  response.data);
+      this.setState({
+        warehouses: [...this.state.warehouses, response.data]
+      })
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        {/* <AddWarehouse />
       <EditWarehouse /> */}
-      <ListWarehouses />
-    </div>
-  );
+        <ListWarehouses 
+        warehouses={this.state.warehouses}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
