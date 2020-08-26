bedd634dfa2e7e9bdc7fd0dfa6838e34dab34974
bpun1p <barry_pun@hotmail.com>
2020-08-26 14:24:18 -0700

    delete confirmation component and basic router settings

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import "./styles/App.css";
 import Header from "./components/Header";
 import AddWarehouse from "./components/AddWarehouse";
 import EditWarehouse from "./components/EditWarehouse";
+import DeleteConfirm from "./components/DeleteConfirm";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header />
       <AddWarehouse />
-      <EditWarehouse />
+      <EditWarehouse /> */}
+      <DeleteConfirm/>
     </div>
   );
 }
