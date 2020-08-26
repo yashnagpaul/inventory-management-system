81ef2491c1a45d500fee90d7f62da4e166954362
yashnagpaul <65575697+yashnagpaul@users.noreply.github.com>
2020-08-25 17:58:54 -0700

    buttons, textfields CSS changes ++ edit warehouse component created

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./styles/App.css";
 import Header from "./components/Header";
+import AddWarehouse from "./components/AddWarehouse";
+import EditWarehouse from "./components/EditWarehouse";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AddWarehouse />
+      <EditWarehouse />
     </div>
   );
 }
