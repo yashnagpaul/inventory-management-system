0da6f7126c7f11ef43e154d1bcebe7cee84265e5
Yash Nagpaul <yash.nagpaul@gmail.com>
2020-08-29 18:21:16 -0700

    comments added in EditWarehouse.jsx

--- client/src/components/EditWarehouse.jsx
+++ client/src/components/EditWarehouse.jsx
@@ -21,7 +21,6 @@ class EditWarehouse extends React.Component {
     },
   };
 
-<<<<<<< HEAD
   // componentDidMount() {
   //   axios.get("http://localhost:8080/api/warehouses").then((response) => {
   //     response.data
@@ -42,28 +41,6 @@ class EditWarehouse extends React.Component {
   //       );
   //   });
   // }
-=======
-  componentDidMount() {
-    axios.get("http://localhost:8080/api/warehouses").then((response) => {
-      response.data
-        .filter((warehouse) => warehouse.id !== this.match.url)
-        .then((result) =>
-          this.setState({
-            itemDetails: {
-              name: result.name,
-              address: result.address,
-              city: result.city,
-              country: result.country,
-              contactName: result.contact.name,
-              position: result.contact.position,
-              number: result.contact.number,
-              emaiil: result.contact.email,
-            },
-          })
-        );
-    });
-  }
->>>>>>> master
 
   saveHandler(event) {
     event.preventDefault();
